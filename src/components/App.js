e87f186c32d55fdb1403f0f09c50b295ad4566d7
Harsh Gupta <41621579+harsh-95@users.noreply.github.com>
2020-04-28 11:44:56 +0530

    fetched Exercises and modified state

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Footer } from "./Layouts";
 import Exercise from "./Exercises/";
+import { muscles, exercises } from "../store";
+
+fetchExercisesByMuscles = () => {
+  return Object.entries(
+    exercises.reduce((exercises, exercise) => {
+      const { muscles } = exercise;
+
+      exercises[muscles] = exercises[muscles]
+        ? [...exercises[muscles], exercise]
+        : [exercise];
+      return exercises;
+    }, {})
+  );
+};
 
 export default function App() {
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    setExercises(fetchExercisesByMuscles());
+  }, []);
+
   return (
     <div>
       <Header />
