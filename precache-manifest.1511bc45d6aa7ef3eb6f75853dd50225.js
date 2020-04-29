bfcb907650b4866685098f9386dce5701dd57a23
Harsh Gupta <harshgupta951114@gmail.com>
2020-04-29 23:38:15 +0530

    Updates

---
+++ precache-manifest.1511bc45d6aa7ef3eb6f75853dd50225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364c3cb21c6901684f09266ba8f78947",
+    "url": "/react-material-ui/index.html"
+  },
+  {
+    "revision": "a97a89a34f85c9042801",
+    "url": "/react-material-ui/static/css/main.20d85443.chunk.css"
+  },
+  {
+    "revision": "47907c6a2b170262bd54",
+    "url": "/react-material-ui/static/js/2.1180bdca.chunk.js"
+  },
+  {
+    "revision": "a97a89a34f85c9042801",
+    "url": "/react-material-ui/static/js/main.1c5318d1.chunk.js"
+  },
+  {
+    "revision": "3064aa741d887559dbbf",
+    "url": "/react-material-ui/static/js/runtime~main.d1d13c8a.js"
+  }
+]);
